d7fb1a692c06ee6d8ebffb83b41d8c7dd504d335
Simon <simon@skvara.eu>
2026-06-28 21:15:50 +0200

    removing maintainer

--- Assets/Plugins/CodeStage/Maintainer/Editor/Scripts/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿#region copyright
-// -------------------------------------------------------
-// Copyright (C) Dmitriy Yukhanov [https://codestage.net]
-// -------------------------------------------------------
-#endregion
-
-[assembly:System.Runtime.CompilerServices.InternalsVisibleTo("CodeStage.Maintainer.Debug")]
-[assembly:System.Runtime.CompilerServices.InternalsVisibleTo("CodeStage.Maintainer.Tests")]
-[assembly:System.Runtime.CompilerServices.InternalsVisibleTo("CodeStage.Maintainer.Demo.Editor")]
